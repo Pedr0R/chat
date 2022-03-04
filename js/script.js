a61454389d62b169b286b803702dd72f461e1f3f
Pedro Ramos <90324497+Pedr0R@users.noreply.github.com>
2022-03-04 19:45:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-inputMessage = document.getElementById('chat-input')
+let chatBox = document.querySelector('#chat-box');
+let sendButton = document.querySelector('#send-message-button');
 
-document.appendChild(m)
+
+function sendMessage() {
+  let textInput = document.getElementById('chat-input');
+  let messageDiv = document.createElement('div');
+  let message = document.createTextNode(textInput.value)
+  messageDiv.appendChild(message);
+  chatBox.appendChild(messageDiv);
+  textInput.value = "";
+} 
+
+sendButton.addEventListener('click', function() {
+  sendMessage()
+});
